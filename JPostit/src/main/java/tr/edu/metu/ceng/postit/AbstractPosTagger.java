390ab3250687247984b9d923259cc50955380e55
tugcemoral <tugcem.oral@gmail.com>
2014-05-30 19:37:32 +0300

    JPost-it PoS Tagger in Turkish is initialized.

---
+++ JPostit/src/main/java/tr/edu/metu/ceng/postit/AbstractPosTagger.java
@@ -0,0 +1,15 @@
+package tr.edu.metu.ceng.postit;
+
+public abstract class AbstractPosTagger implements IPosTagger{
+
+	@Override
+	public void convert(String analysis) {
+		
+	}
+	
+	@Override
+	public void tag(String sentence) {
+		
+	}
+	
+}
---
+++ JPostit/src/main/java/tr/edu/metu/ceng/postit/IPosTagger.java
@@ -0,0 +1,10 @@
+package tr.edu.metu.ceng.postit;
+
+public interface IPosTagger {
+
+	public void convert(String analysis);
+	
+	public void tag(String sentence);
+	
+	
+}
