7246bd5eab1188cedc3d829e278fedc7c72457b2
tugcemoral <tugcem.oral@gmail.com>
2014-06-13 00:49:00 +0300

    minor bug on PosTagger3 is fixed.

--- JPostit/src/main/java/tr/edu/metu/ceng/postit/tagger/PosTagger3.java
+++ JPostit/src/main/java/tr/edu/metu/ceng/postit/tagger/PosTagger3.java
@@ -1,64 +1,24 @@
 package tr.edu.metu.ceng.postit.tagger;
 
-import java.util.List;
-import java.util.Vector;
-
 public class PosTagger3 extends AbstractPosTagger {
 
 	@Override
 	protected String extractTag(String tagSentence) {
 
-		List<String> tags = new Vector<String>();
+		// find model1 tag.
+		String tag = (tagSentence.contains("^DB") ? tagSentence.substring(
+				tagSentence.lastIndexOf("^DB") + ("^DB+".length()))
+				.split("\\W")[0] : tagSentence.split("\\W")[0]);
 
-		// check for modified tag.
-		if (tagSentence.contains("^DB+")) {
-			// split tag locations accordingly.
-			String[] splittedTagLocations = tagSentence.split("\\^DB\\+");
+		// also check for nouns.
+		String tagWithNoun = tag.equalsIgnoreCase("noun") ? String.format(
+				"%s%s", tag,
+				tagSentence.substring(tagSentence.lastIndexOf("+"))) : tag;
 
-			// get first splitted element's first splitted element(first tag).
-			String firstTag = splittedTagLocations[0].split("\\W")[0];
-			// add to tags.
-			tags.add(firstTag);
+		// finally add stem information
+		String wholeTag = tagSentence.contains("^DB") ? String.format("%s+%s",
+				tagSentence.split("\\W")[0], tagWithNoun) : tagWithNoun;
 
-			// extract next tags.
-			for (int i = 1; i < splittedTagLocations.length; i++) {
-				String nextTag = splittedTagLocations[i].split("\\W")[0];
-				if (!tags.contains(nextTag)) {
-					tags.add(nextTag);
-				}
-			}
-
-		} else {
-			// only add single tag.
-			tags.add(tagSentence.split("\\W")[0]);
-		}
-
-		// if the list itself contains "noun", add additional case information.
-		if (containsIgnoreCase(tags, "noun")) {
-			tags.add(tagSentence.substring(tagSentence.lastIndexOf("+")+1));
-		}
-		return stringifyTagList(tags);
+		return wholeTag;
 	}
-
-	private String stringifyTagList(List<String> tags) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < tags.size(); i++) {
-			if (i < tags.size() - 1) {
-				sb.append(tags.get(i)).append("+");
-			} else {
-				sb.append(tags.get(i));
-			}
-		}
-		return sb.toString();
-	}
-
-	private boolean containsIgnoreCase(List<String> list, String s) {
-		for (String element : list) {
-			if (element.equalsIgnoreCase(s)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 }
